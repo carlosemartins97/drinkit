f09aad55b4dc7b8946f4df660a2db3cb142d6d5a
Carlos Martins <carlosemartins97@gmail.com>
2021-03-01 17:31:20 -0300

    Add countdown component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head';
+
 import { CompletedChallanges } from '../components/CompletedChallanges';
+import { Countdown } from '../components/Countdown';
 
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+
 
 import styles from '../styles/pages/Home.module.css'
 
@@ -11,12 +14,17 @@ export default function Home() {
     <div>
 
       <div className={styles.container}>
+        <Head>
+          <title>Início | DrinkIt </title>
+        </Head>
+        
         <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallanges />
+          <Countdown />
         </div>
 
         <div></div>
